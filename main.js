3bdf94a061e0657c28f05d7d1f9e97fa188ce87d
cultofmetatron <cultofmetatron@aumlogic.com>
2013-06-04 16:31:43 -0700

    codemirror

--- main.js
+++ main.js
@@ -3,17 +3,35 @@ derby.use(require('./ui'));
 var app = derby.createApp(module);
 
 app.ready(function(model) {
-  //var myCodeMirror = CodeMirror.fromTextArea(document.getElementsByTagName('textarea')[0]);
-
-
+  var setCodemirror = function() {
+    setTimeout( function() {
+      var myCodeMirror = CodeMirror.fromTextArea(document.getElementsByTagName('textarea')[0]);
+    }, 100);
+  };
+  //setCodemirror();
   model.on('set','dbName',function(path,object){
+    //setCodemirror();
   });
-  model.on('set','collectionBox', function(path,obj){
+
+  model.on('set','collectionBoxName', function(path,obj){
+    setCodemirror();
     app.page.render();
   });
   model.on('set','collections',function(path,object){
+    console.log('path', path, 'object', object);
     console.log('!',model.get('collections'), app);
-    app.page.render();
+    //setCodemirror();
+    app.page.render('texteditor');
+
+  });
+
+
+
+
+  model.on('change', 'dbName', function(path, object) {
+    console.log('collection boxname changed!!');
+    console.log('textarea', document.getElementsByTagName('textarea')[0]);
+    var myCodeMirror = CodeMirror.fromTextArea(document.getElementsByTagName('textarea')[0]);
   });
 
   app.changeDatabase = function(e, element, next) {
@@ -24,6 +42,6 @@ app.ready(function(model) {
   app.toggleCollection = function (e, element, next){
     app.model.set('collectionBoxName',e.target.dataset.value);
     next();
-  }
+  };
 });
 
